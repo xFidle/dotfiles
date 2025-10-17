bc2dd4b166f1548c82d99c954b811362e5eb2870
Michał Szwejk <mszwejk1@gmail.com>
2025-10-17 16:39:56 +0200

    add fzf-lua bindings

--- nvim/.config/nvim/lua/plugins/fzf-lua.lua
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -6,6 +6,15 @@ return {
     },
     keys = {
         { '<leader>ff', function() require('fzf-lua').files() end },
-        { '<leader>fg', function() require('fzf-lua').live_grep() end }
+        { '<leader>fg', function() require('fzf-lua').live_grep() end },
+        { '<leader>fc', function() require('fzf-lua').files({cwd = vim.fn.expand('$HOME/dotfiles')}) end },
+        { '<leader>fh', function() require('fzf-lua').helptags() end },
+        { '<leader>fk', function() require('fzf-lua').keymaps() end },
+        { '<leader>fb', function() require('fzf-lua').builtin() end },
+        { '<leader>fw', function() require('fzf-lua').grep_cword() end },
+        { '<leader>fW', function() require('fzf-lua').grep_cWORD() end },
+        { '<leader>fo', function() require('fzf-lua').oldfiles() end },
+        { '<leader><leader>', function() require('fzf-lua').buffers() end },
+        { '<leader>/', function() require('fzf-lua').lgrep_curbuf() end }
     }
 }
