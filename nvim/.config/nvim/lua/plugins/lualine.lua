f82a71e9af8c58196801deac894e29c448149c51
Michał Szwejk <mszwejk1@gmail.com>
2025-10-23 00:50:50 +0200

    add lsp status to lualine

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,42 +1,39 @@
 return {
-    'nvim-lualine/lualine.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    opts = {
-        options = {
-            icons_enabled = true,
-            globalstatus = true,
-            component_separators = { left = '', right = ''},
-            section_separators = { left = '', right = ''},
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  opts = {
+    options = {
+      icons_enabled = true,
+      globalstatus = true,
+      component_separators = { left = '', right = '' },
+      section_separators = { left = '', right = '' },
+    },
+    sections = {
+      lualine_a = { 'mode' },
+      lualine_b = {
+        { 'branch', icon = '' },
+        'diff',
+      },
+      lualine_c = {
+        function()
+          local filename = vim.fn.expand('%:t')
+          local extension = vim.fn.expand('%:e')
+          local icon, color = require('nvim-web-devicons').get_icon_color(filename, extension)
+          local hl_group = 'LualineFileIconColor' .. extension
+          vim.api.nvim_set_hl(0, hl_group, { fg = color })
+          return '%#' .. hl_group .. '#' .. icon .. '%* ' .. filename
+        end,
+        'diagnostics',
+      },
+      lualine_x = { { 'lsp_status', icon = { ' ', align = 'right' } } },
+      lualine_y = {
+        {
+          function() return vim.fn.fnamemodify(vim.fn.getcwd(), ':t') end,
+          icon = { ' ', align = 'right' },
         },
-        sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 
-                { 'branch', icon='' },
-                'diff',
-            },
-            lualine_c = { 
-                function ()
-                    local filename = vim.fn.expand('%:t')
-                    local extension = vim.fn.expand('%:e')
-                    local icon, color = require('nvim-web-devicons').get_icon_color(filename, extension)
-                    local hl_group = 'LualineFileIconColor' .. extension
-                    vim.api.nvim_set_hl(0, hl_group, { fg = color })
-                    return '%#' .. hl_group .. '#' .. icon .. '%* ' .. filename
-                end,
-                'diagnostics'
-            },
-            -- TODO: Configure lsp to see diagnostics
-            lualine_x = { 
-                {
-                    function ()
-                        local directory = vim.fn.fnamemodify(vim.fn.getcwd(), ':t')
-                        return directory
-                    end,
-                    icon = {  ' ', align = 'right' }
-                },
-            },
-            lualine_y = { { 'datetime', icon = { ' ', align = 'right' }, style='%H:%M:%S' } },
-            lualine_z = { 'progress', 'location' },
-        },
-    }
+        { 'datetime', icon = { ' ', align = 'right' }, style = '%H:%M:%S' },
+      },
+      lualine_z = { 'progress', 'location' },
+    },
+  },
 }
