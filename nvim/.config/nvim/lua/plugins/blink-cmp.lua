e4fb952659eafcf9a50a283a9fddc2d80edfc0e4
Michał Szwejk <mszwejk1@gmail.com>
2025-10-31 17:31:28 +0100

    setup tmux

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -24,6 +24,5 @@ return {
     },
     sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
     fuzzy = { implementation = 'prefer_rust_with_warning' },
-    opts_extend = { 'sources.default' },
   },
 }
