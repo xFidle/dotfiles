532dbf1f444753d683407b88222cd34f75821663
Michał Szwejk <mszwejk1@gmail.com>
2026-01-31 17:51:58 +0100

    improve switch-theme script

--- bin/.local/bin/switch-theme.sh
+++ bin/.local/bin/switch-theme.sh
@@ -3,10 +3,9 @@ set -e
 
 THEMES=("tokyonight" "vague")
 
-theme=$1
-current=current-theme
-file=$HOME/.config/theme/$current
-
+THEME=$1
+CURRENT=current-theme
+FILE=$HOME/.config/theme/$CURRENT
 
 if [[ "$#" -ne 1 ]]; then
     echo "Theme name not provided" >&2
@@ -15,7 +14,7 @@ fi
 
 found=false
 for item in ${THEMES[@]}; do 
-    if [[ "$theme" == "$item" ]]; then
+    if [[ "$THEME" == "$item" ]]; then
         found=true
         break
     fi
@@ -26,27 +25,30 @@ if ! $found ; then
     exit 2
 fi
 
-echo $theme > $file.conf 
-echo "Theme '$theme' set in config file '$file'" >&1
+echo $THEME > $FILE.conf 
+echo "Theme '$THEME' set in config file '$FILE'" >&1
 
-kitten themes $theme
-echo "KITTY: theme '$theme' set" >&1
+kitten themes $THEME 
+echo "KITTY: theme '$THEME' set" >&1
 
-ln -sf $(realpath --relative-to="$HOME/.config/tmux" "$HOME/.config/tmux/themes/$theme.conf") \
-    "$HOME/.config/tmux/$current.conf"
+TMUX_PATH="$HOME/.config/tmux"
+ln -sf $(realpath --relative-to="$TMUX_PATH" "$TMUX_PATH/themes/$THEME.conf") \
+    "$TMUX_PATH/$CURRENT.conf"
 tmux source "$HOME/.config/tmux/tmux.conf"
-echo "TMUX: theme '$theme' set" >&1
+echo "TMUX: theme '$THEME' set" >&1
 
 if [[ -S "$NVIM_SOCKET" ]]; then
-    nvim --server $NVIM_SOCKET --remote-send ":colorscheme $theme<CR>"
-    echo "NVIM: theme '$theme' set" >&1
+    nvim --server $NVIM_SOCKET --remote-send ":colorscheme $THEME<CR>"
+    echo "NVIM: theme '$THEME' set" >&1
 fi
 
-ln -sf $(realpath --relative-to="$HOME/.config/fzf" "$HOME/.config/fzf/themes/$theme.conf") \
-    "$HOME/.config/fzf/$current.conf" 
-echo "FZF: theme '$theme' set" >&1
+FZF_PATH="$HOME/.config/fzf"
+ln -sf $(realpath --relative-to="$FZF_PATH" "$FZF_PATH/themes/$THEME.conf") \
+    "$FZF_PATH/$CURRENT.conf" 
+echo "FZF: theme '$THEME' set" >&1
 
-ln -sf $(realpath --relative-to="$HOME/.config/bat/themes" "$HOME/.config/bat/themes/$theme.tmTheme") \
-    "$HOME/.config/bat/themes/$current.tmTheme"
+BAT_PATH="$HOME/.config/bat"
+ln -sf $(realpath --relative-to="$BAT_PATH/themes" "$BAT_PATH/themes/$THEME.tmTheme") \
+    "$BAT_PATH/themes/$CURRENT.tmTheme"
 bat cache --build > /dev/null 2>&1
-echo "BAT: theme '$theme' set" >&1
+echo "BAT: theme '$THEME' set" >&1
