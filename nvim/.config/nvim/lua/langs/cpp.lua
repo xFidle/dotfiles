2227d2ac52c6b487519cf9964a77191d417d9124
Michał Szwejk <mszwejk1@gmail.com>
2026-06-14 18:23:27 +0200

    add cpp config

---
+++ nvim/.config/nvim/lua/langs/cpp.lua
@@ -0,0 +1,53 @@
+---@type LangSpec
+local lang = {
+  filetypes = { 'c', 'cpp' },
+  mason_tools = { 'clangd', 'clang-format', 'cmakelang', 'cmakelint' },
+  treesitter = { 'c', 'cpp', 'cmake' },
+  servers = {
+    clangd = {
+      init_options = {
+        usePlaceholders = false,
+        completeUnimported = true,
+        clangdFileStatus = true,
+      },
+      cmd = {
+        'clangd',
+        '--background-index',
+        '--clang-tidy',
+        '--header-insertion=iwyu',
+        '--completion-style=detailed',
+        '--function-arg-placeholders',
+        '--fallback-style=llvm',
+      },
+      root_markers = {
+        'compile_commands.json',
+        'compile_flags.txt',
+        'configure.ac',
+        'Makefile',
+        'configure.ac',
+        'meson.build',
+        'build.ninja',
+        '.git',
+      },
+      capabilities = {
+        offsetEncoding = { 'utf-16' },
+      },
+    },
+  },
+  keymaps = {
+    clangd = {
+      { 'n', 'grH', '<CMD>LspClangdSwitchSourceHeader<CR>', { desc = 'Switch Source/Header (C/C++)' } },
+    },
+  },
+  formatters_by_ft = { 'clang-format' },
+}
+
+---@type LazySpec[]
+---TODO: examine cmake-tools.nvim
+local plugins = {}
+
+---@type LangBundle
+return {
+  lang = lang,
+  plugins = plugins,
+}
