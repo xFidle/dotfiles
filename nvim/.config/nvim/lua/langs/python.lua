26f587033f9d2fd660d7147b02a3ace2e619fdeb
Michał Szwejk <mszwejk1@gmail.com>
2026-06-15 23:06:42 +0200

    add python support

---
+++ nvim/.config/nvim/lua/langs/python.lua
@@ -0,0 +1,39 @@
+---@type LangSpec
+local lang = {
+  filetypes = { 'python' },
+  mason_tools = { 'pyright', 'ruff' },
+  treesitter = { 'python' },
+  servers = {
+    pyright = {
+      settings = {
+        pyright = {
+          disableOrganizeImports = true,
+        },
+        python = {
+          analysis = {
+            ignore = { '*' },
+            autoSearchPaths = true,
+            useLibraryCodeForTypes = true,
+          },
+        },
+      },
+    },
+    ruff = {
+      init_options = {
+        settings = {
+          configurationPreference = 'filesystemFirst',
+          logLevel = 'error',
+        },
+      },
+      on_attach = function(client)
+        client.server_capabilities.hoverProvider = false
+      end,
+    },
+  },
+  formatters_by_ft = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports' },
+}
+
+---@type LangBundle
+return {
+  lang = lang,
+}
