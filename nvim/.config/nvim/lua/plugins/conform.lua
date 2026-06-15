90d3834a9d27bb7f438295d86cd04ed177d34cdf
Michał Szwejk <mszwejk1@gmail.com>
2026-06-15 18:31:45 +0200

    add toggle-atuoformat keymaps

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -1,12 +1,45 @@
+---@param scope 'buffer'|'global'
+local function toggle_autoformat(scope)
+  local is_buffer = scope == 'buffer'
+  local var = is_buffer and vim.b or vim.g
+  local enable_command = 'FormatEnable'
+  local disable_command = is_buffer and 'FormatDisable!' or 'FormatDisable'
+
+  if var.disable_autoformat then
+    vim.cmd(enable_command)
+    vim.notify('Enabled auto-format witch scope: ' .. scope, vim.log.levels.INFO)
+  else
+    vim.cmd(disable_command)
+    vim.notify('Disabled auto-format with scope: ' .. scope, vim.log.levels.INFO)
+  end
+end
+
 return {
   'stevearc/conform.nvim',
   event = 'BufWritePre',
   opts = {
     formatters = require('core.registry').get_formatters(),
     formatters_by_ft = require('core.registry').get_formatters_by_ft(),
-    format_on_save = { timeout = 500, lsp_format = 'fallback' },
+    format_on_save = function(bufnr)
+      if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then return end
+      return { timeout = 500, lsp_format = 'fallback' }
+    end,
   },
   keys = {
+    {
+      '<leader>ct',
+      function()
+        toggle_autoformat('buffer')
+      end,
+      desc = 'Toggle auto-format in current buffer',
+    },
+    {
+      '<leader>cT',
+      function()
+        toggle_autoformat('global')
+      end,
+      desc = 'Toggle auto-format globally',
+    },
     {
       '<leader>cf',
       function()
@@ -22,4 +55,25 @@ return {
       desc = 'Display conform info',
     },
   },
+  config = function(_, opts)
+    require('conform').setup(opts)
+
+    vim.api.nvim_create_user_command('FormatDisable', function(args)
+      if args.bang then
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = 'Disable autoformat on save',
+      bang = true,
+    })
+
+    vim.api.nvim_create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = 'Enable autoformat on-save',
+    })
+  end,
 }
