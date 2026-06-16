8987f53aab5eac85153146f40472018c29e7b25d
Michał Szwejk <mszwejk1@gmail.com>
2026-06-16 15:59:56 +0200

    change blink-cmp config

--- nvim/.config/nvim/lua/plugins/blink-cmp.lua
+++ nvim/.config/nvim/lua/plugins/blink-cmp.lua
@@ -1,3 +1,71 @@
+local feat_options = {
+  keymap = {
+    preset = 'default',
+    ['<C-k>'] = { 'select_prev', 'fallback' },
+    ['<C-j>'] = { 'select_next', 'fallback' },
+  },
+  appearance = {
+    use_nvim_cmp_as_default = false,
+    nerd_font_variant = 'mono',
+  },
+  cmdline = {
+    completion = {
+      menu = { auto_show = false },
+      ghost_text = { show_with_menu = false },
+    },
+  },
+  completion = {
+    menu = {
+      auto_show = true,
+      auto_show_delay_ms = 500,
+      draw = {
+        treesitter = { 'lsp' },
+      },
+    },
+    list = {
+      selection = {
+        preselect = true,
+        auto_insert = false,
+      },
+    },
+    accept = {
+      auto_brackets = { enabled = true },
+    },
+    documentation = {
+      auto_show = true,
+      auto_show_delay_ms = 500,
+      treesitter_highlighting = true,
+    },
+    ghost_text = { enabled = true, show_without_menu = false },
+  },
+  sources = {
+    default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer' },
+    providers = {
+      dadbod = {
+        name = 'Dadbod',
+        module = 'vim_dadbod_completion.blink',
+      },
+      lazydev = {
+        name = 'LazyDev',
+        module = 'lazydev.integrations.blink',
+        score_offset = 100,
+      },
+    },
+  },
+  signature = { enabled = true },
+  fuzzy = { implementation = 'rust' },
+}
+
+local ui_options = {
+  completion = {
+    menu = {
+      draw = {
+        columns = { { 'label', 'label_description', gap = 1 }, { 'kind_icon', 'kind', 'source_name', gap = 1 } },
+      },
+    },
+  },
+}
+
 return {
   {
     'saghen/blink.compat',
@@ -7,67 +75,14 @@ return {
   },
   {
     'saghen/blink.cmp',
-    dependencies = { 'rafamadriz/friendly-snippets', 'micangl/cmp-vimtex' },
-    version = '1.*',
-    opts = {
-      keymap = {
-        preset = 'default',
-        ['<C-k>'] = { 'select_prev', 'fallback' },
-        ['<C-j>'] = { 'select_next', 'fallback' },
-      },
-      cmdline = {
-        completion = { menu = { auto_show = false } },
-      },
-      appearance = { nerd_font_variant = 'mono' },
-      completion = {
-        menu = {
-          winhighlight = 'NormalFloat:BlinkCmpMenu',
-          scrollbar = true,
-          draw = {
-            columns = {
-              { 'kind_icon', 'label', gap = 1 },
-              { 'kind' },
-            },
-          },
-        },
-        documentation = {
-          auto_show = true,
-          treesitter_highlighting = true,
-          auto_show_delay_ms = 500,
-          window = {
-            winhighlight = 'NormalFloat:BlinkCmpDoc',
-          },
-        },
-      },
-      signature = {
-        enabled = true,
-        window = {
-          winhighlight = 'NormalFloat:BlinkCmpSignatureHelp',
-        },
-      },
-      sources = {
-        default = { 'lazydev', 'lsp', 'path', 'snippets', 'buffer', 'vimtex' },
-        per_filetype = {
-          sql = { 'snippets', 'dadbod', 'buffer' },
-        },
-        providers = {
-          dadbod = {
-            name = 'Dadbod',
-            module = 'vim_dadbod_completion.blink',
-          },
-          lazydev = {
-            name = 'LazyDev',
-            module = 'lazydev.integrations.blink',
-            score_offset = 100,
-          },
-          vimtex = {
-            name = 'vimtex',
-            module = 'blink.compat.source',
-            score_offset = 100,
-          },
-        },
-      },
-      fuzzy = { implementation = 'prefer_rust_with_warning' },
+    dependencies = {
+      'saghen/blink.lib',
+      'rafamadriz/friendly-snippets',
     },
+    build = function()
+      ---@diagnostic disable-next-line
+      require('blink.cmp').build():pwait()
+    end,
+    opts = vim.tbl_deep_extend('error', feat_options, ui_options),
   },
 }
