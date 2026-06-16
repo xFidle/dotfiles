cf91d39e3f829133955264fe847778bf1a845952
Michał Szwejk <mszwejk1@gmail.com>
2026-06-16 22:18:02 +0200

    enable cursorline in floating windows

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -4,3 +4,15 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
+vim.api.nvim_create_autocmd('WinEnter', {
+  group = vim.api.nvim_create_augroup('cursorline', { clear = true }),
+  desc = 'Enable cursorline in floating windows',
+  callback = function()
+    local win = vim.api.nvim_get_current_win()
+    vim.schedule(function()
+      if not vim.api.nvim_win_is_valid(win) then return end
+      vim.wo[win].cursorline = true
+    end)
+  end,
+})
