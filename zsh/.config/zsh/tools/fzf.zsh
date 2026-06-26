0a514aa9526327b5e79a5be4522ca1fced562843
Michał Szwejk <mszwejk1@gmail.com>
2026-06-27 01:00:22 +0200

    finish fzf config i guess

--- zsh/.config/zsh/tools/fzf.zsh
+++ zsh/.config/zsh/tools/fzf.zsh
@@ -1,12 +1,18 @@
 #!/usr/bin/env zsh
 
-() {
+_fzf() {
   local fzf="${XDG_CONFIG_HOME}/fzf"
-
   export FZF_DEFAULT_OPTS="$(cat "$fzf/config" "$fzf/themes/current")"
-  export FZF_CTRL_T_OPTS="$(cat "$fzf/ctrl-t")"
-  export FZF_ALT_C_OPTS="$(cat "$fzf/alt-c")"
-
+  export FZF_CTRL_T_OPTS="--preview 'bat --color=always --style=plain,numbers {}'"
+  export FZF_ALT_C_OPTS="--preview 'lsd --tree --color=always --icon=always {}'"
   export FZF_DEFAULT_COMMAND="fd --type f --hidden --strip-cwd-prefix"
   export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 }
+
+_fzf-tab() {
+  zstyle ':fzf-tab:*' use-fzf-default-opts yes
+}
+
+autoload -U compinit
+compinit
+_fzf && _fzf-tab
