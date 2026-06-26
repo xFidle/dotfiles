30bcc444c0c65a8420161f236c58a062ee579261
Michał Szwejk <mszwejk1@gmail.com>
2026-06-26 18:24:33 +0200

    wrap fzf setup in anonymous func

--- zsh/.config/zsh/tools/fzf.zsh
+++ zsh/.config/zsh/tools/fzf.zsh
@@ -1,7 +1,8 @@
 #!/usr/bin/env zsh
 
-local fzf_path="${XDG_CONFIG_HOME}/fzf"
-local config="$fzf_path/config"
-local theme="$fzf_path/themes/current"
-
-export FZF_DEFAULT_OPTS="$(cat $config $theme)"
+() {
+    local fzf_path="${XDG_CONFIG_HOME}/fzf"
+    local config="$fzf_path/config"
+    local theme="$fzf_path/themes/current"
+    export FZF_DEFAULT_OPTS="$(cat $config $theme)"
+}
