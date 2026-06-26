1f1dc8374ce9ea7077036fdda9c6297be13e55c5
Michał Szwejk <mszwejk1@gmail.com>
2026-06-26 18:54:22 +0200

    add more aliases

--- zsh/.config/zsh/core/aliases.zsh
+++ zsh/.config/zsh/core/aliases.zsh
@@ -1,5 +1,14 @@
 #!/usr/bin/env zsh
 
-alias ls='lsd'
+alias ..='cd ..'
 alias vi='nvim'
 alias vim='nvim'
+
+alias ls='lsd'
+alias tree='lsd --tree'
+alias cat='bat'
+alias find='fd'
+alias grep='rg --color=auto'
+
+alias diff='diff --color=auto'
+alias df='df -h'
