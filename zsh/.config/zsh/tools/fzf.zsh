2a24871bb9a0dd31ce279a6807196c459d2efa16
Michał Szwejk <mszwejk1@gmail.com>
2026-06-26 18:54:31 +0200

    customize fzf a bit

--- zsh/.config/zsh/tools/fzf.zsh
+++ zsh/.config/zsh/tools/fzf.zsh
@@ -1,8 +1,12 @@
 #!/usr/bin/env zsh
 
 () {
-    local fzf_path="${XDG_CONFIG_HOME}/fzf"
-    local config="$fzf_path/config"
-    local theme="$fzf_path/themes/current"
-    export FZF_DEFAULT_OPTS="$(cat $config $theme)"
+  local fzf="${XDG_CONFIG_HOME}/fzf"
+
+  export FZF_DEFAULT_OPTS="$(cat "$fzf/config" "$fzf/themes/current")"
+  export FZF_CTRL_T_OPTS="$(cat "$fzf/ctrl-t")"
+  export FZF_ALT_C_OPTS="$(cat "$fzf/alt-c")"
+
+  export FZF_DEFAULT_COMMAND="fd --type f --hidden --strip-cwd-prefix"
+  export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 }
