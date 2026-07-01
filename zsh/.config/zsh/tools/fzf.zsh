388110f111c9b162d38840fbaab54498358c9a1c
Michał Szwejk <mszwejk1@gmail.com>
2026-07-01 21:30:05 +0200

    fix fzf switch theme

--- zsh/.config/zsh/tools/fzf.zsh
+++ zsh/.config/zsh/tools/fzf.zsh
@@ -1,6 +1,7 @@
 _fzf() {
   local fzf="${XDG_CONFIG_HOME}/fzf"
-  export FZF_DEFAULT_OPTS="$(cat "$fzf/config" "$fzf/themes/current")"
+  export FZF_DEFAULT_OPTS="$(cat "${fzf}/config")"
+  export FZF_DEFAULT_OPTS_FILE="${fzf}/themes/current"
   export FZF_CTRL_T_OPTS="--preview 'bat --color=always --style=plain,numbers {}'"
   export FZF_ALT_C_OPTS="--preview 'lsd --tree --color=always --icon=always {}'"
   export FZF_DEFAULT_COMMAND="fd --type f --hidden --strip-cwd-prefix --exclude .git"
